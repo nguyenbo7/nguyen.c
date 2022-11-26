fccbeb66579c144e4dcbb40d01298d08a5858bce
Nguyen Chau <nguyenchaubo7@gmail.com>
2022-11-26 14:42:00 -0800

    task 02:  update nav bar (phone version), new design 	1.  only show the name website on the nav bar 	2.  add hamburger (menu toggle) 	3.  show the menu img when menu toggle is clicked

---
+++ app.js
@@ -0,0 +1,25 @@
+const menu      = document.querySelector(".nav__menu");
+const menuLinks = document.querySelectorAll(".nav__menu-link");
+const hamburger = document.querySelector(".nav__toggle");
+const closeIcon = document.querySelector(".icon__close");
+const menuIcon  = document.querySelector(".icon__menu");
+
+function toggleMenu() {
+  if (menu.classList.contains("nav__menu-show")) {
+    menu.classList.remove("nav__menu-show");
+    closeIcon.style.display = "none";
+    menuIcon.style.display  = "block";
+  } else {
+    menu.classList.add("nav__menu-show");
+    closeIcon.style.display = "block";
+    menuIcon.style.display  = "none";
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu);
+
+menuLinks.forEach( 
+    function(menuEachLink) { 
+      menuEachLink.addEventListener("click", toggleMenu);
+    }
+)
