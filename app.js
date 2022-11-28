ecd3aa3c530cdfbe5d3649bdefc63e3e90369baa
Nguyen Chau <nguyenchaubo7@gmail.com>
2022-11-27 22:11:09 -0800

    task 04:  working on home section, finish the home info (accordion) 	1.  change nav bar, home content width from % to px 	2.  delete height for the home info (mobile version) 	3.  change diff icon website 	4.  change the tab space 	5.  finish the accordion with js

--- app.js
+++ app.js
@@ -3,8 +3,8 @@
 const menu      = document.querySelector(".nav__menu");
 const menuLinks = document.querySelectorAll(".nav__menu-link");
 const hamburger = document.querySelector(".nav__toggle");
-const closeIcon = document.querySelector(".icon__close");
-const menuIcon  = document.querySelector(".icon__menu");
+const closeIcon = document.querySelector(".icon__nav-close");
+const menuIcon  = document.querySelector(".icon__nav-menu");
 
 function toggleMenu() {
   if (menu.classList.contains("nav__menu-show")) {
@@ -28,3 +28,30 @@ menuLinks.forEach(
 
 //  label info toggle
 //  --------------------------------------------------------------
+const accordions = document.querySelectorAll(".home__info");
+
+accordions.forEach((item, index) => {
+    let accor_header = item.querySelector(".info__header");
+
+    accor_header.addEventListener("click", () => {
+        item.classList.toggle("home__info-show");
+
+        if(item.classList.contains("home__info-show"))
+        {
+            item.querySelector(".icon__info").classList.replace("uil-plus", "uil-times");
+        }
+        else{
+            item.querySelector(".icon__info").classList.replace("uil-times", "uil-plus");
+        }
+        removeOpen(index);
+    })
+})
+
+function removeOpen(index1) {
+    accordions.forEach((item2, index2) => {
+        if(index1 != index2){
+            item2.classList.remove("home__info-show");
+            item2.querySelector(".icon__info").classList.replace("uil-times", "uil-plus");
+        }
+    })
+}
